5980753bce43b902930cfcdbbdb319cb5ffea7ef
DaveyAgni <devarchit.agnihotri@icloud.com>
2024-11-09 13:41:11 -0500

    Update FactorialRunner.java

--- FactorialRunner.java
+++ FactorialRunner.java
@@ -2,9 +2,16 @@
 //www.apluscompsci.com
 //Name
 
+import java.util.Scanner;
+
 public class FactorialRunner
 {
 	public static void main ( String[] args )
 	{
+		Scanner kb = new Scanner(System.in);
+		System.out.print("Enter number for the factorial: ");
+		int num = kb.nextInt();
+		Factorial x = new Factorial();
+		x.getFactorial(num);
 	}
 }
