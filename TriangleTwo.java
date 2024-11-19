98b03aa617876098683acc1ccfc81afa50ff1fe9
DaveyAgni <devarchit.agnihotri@icloud.com>
2024-11-19 09:53:31 -0500

    Update TriangleTwo.java

--- TriangleTwo.java
+++ TriangleTwo.java
@@ -1,12 +1,23 @@
-//(c) A+ Computer Science
-//www.apluscompsci.com
-//Name - 
-
 public class TriangleTwo
 {
-	public static String go( int size, String let )
+
+	public static String go( String let, int size)
 	{
-		String output="";
-		return output+"\n";
+	int i = size;
+        int j = 1;
+
+	for(;i >= 0; i--)
+        {
+            for(j=1;j <= i; j++)
+            {
+                System.out.print(let);
+
+            }
+            System.out.print("\n");
+        }
+
+
+	String output="";
+	return output;
 	}
 }
